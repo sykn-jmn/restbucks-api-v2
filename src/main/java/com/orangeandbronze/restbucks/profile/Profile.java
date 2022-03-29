dbbfb84d7321e2c66528bd9ae8b328f1388cbc9d
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2022-03-29 14:08:23 +0800

    Added Profile class

---
+++ src/main/java/com/orangeandbronze/restbucks/profile/Profile.java
@@ -0,0 +1,42 @@
+package com.orangeandbronze.restbucks.profile;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Profile {
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    private final String firstName;
+    private final String lastName;
+
+    public Profile(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return id.equals(profile.id) && firstName.equals(profile.firstName) && lastName.equals(profile.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+}
