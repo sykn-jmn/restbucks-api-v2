8c79e1e625565e782a8ed5ab0c5f218360b7a993
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2022-03-29 23:46:28 +0800

    fixed root

--- src/main/java/com/orangeandbronze/restbucks/RestbucksController.java
+++ src/main/java/com/orangeandbronze/restbucks/RestbucksController.java
@@ -1,30 +1,24 @@
 package com.orangeandbronze.restbucks;
 
 import com.orangeandbronze.restbucks.drinks.DrinkController;
-import com.orangeandbronze.restbucks.orders.Order;
 import com.orangeandbronze.restbucks.orders.OrderController;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping("/api")
 public class RestbucksController {
 
-    RestbucksController () {
-
-    }
-    @GetMapping("/")
-    public EntityModel<Restbucks> all(){
-        return EntityModel.of(new Restbucks(),
-                linkTo(methodOn(DrinkController.class).all()).withRel("drinks"),
+    RestbucksController () { }
+    @GetMapping
+    public RepresentationModel root(){
+        RepresentationModel model = new RepresentationModel();
+        return model.add(linkTo(methodOn(DrinkController.class).all()).withRel("drinks"),
                 linkTo(methodOn(OrderController.class).all()).withRel("orders"));
     }
 
