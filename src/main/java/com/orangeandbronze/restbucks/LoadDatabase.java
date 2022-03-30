78368cd2cf17e62142f194c11038ecf29feefae4
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2022-03-30 17:11:43 +0800

    typo

--- src/main/java/com/orangeandbronze/restbucks/LoadDatabase.java
+++ src/main/java/com/orangeandbronze/restbucks/LoadDatabase.java
@@ -28,7 +28,7 @@ class LoadDatabase {
         return args -> {
             Drink drink1 = new Drink("Americano", "Starbucks Reserve® espresso with hot water", "P250.00", "https://d1ralsognjng37.cloudfront.net/6c716fb9-2137-4a1d-af41-4a2647a8c9f6.jpeg");
             Drink drink2 = new Drink("Cappuccino", "Starbucks Reserve® espresso with steamed milk, topped with a layer of foam", "P350.00", "https://d1ralsognjng37.cloudfront.net/0d2e6fa2-418c-4f6e-bb69-819468967aa5.jpeg");
-            Drink drink5 = new Drink("Chai Tea Latte", "A concentrated blend of full-bodied black teas, black pepper and spicy ginger with notes of cardamom and vanilla, lightly sweetened. Topped with hot water, steamed milk and foam.", "2350.00", "https://d1ralsognjng37.cloudfront.net/c49d796e-07ea-46bd-8bd8-b496a8851e38.jpeg");
+            Drink drink5 = new Drink("Chai Tea Latte", "A concentrated blend of full-bodied black teas, black pepper and spicy ginger with notes of cardamom and vanilla, lightly sweetened. Topped with hot water, steamed milk and foam.", "P235.00", "https://d1ralsognjng37.cloudfront.net/c49d796e-07ea-46bd-8bd8-b496a8851e38.jpeg");
 
             drinkRepository.save(drink1);
             drinkRepository.save(drink2);
